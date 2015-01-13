8f1418ef404ae7e32026e9589f0573142a094ced
Azhar Deen <ahmdeen@gmail.com>
2015-01-13 15:31:39 -0800

    added 01-Freqwords.py

---
+++ 01-FreqWords.py
@@ -0,0 +1,43 @@
+"""
+Frequent Words 
+
+Find the most frequent k-mers in a string.
+
+Given: A DNA string Text and an integer k.
+
+Return: All most frequent k-mers in Text (in any order).
+
+"""
+
+
+#Inputs ---------------------------------------------------
+#----------------------------------------------------------
+Text = raw_input()
+k = int(raw_input())
+#----------------------------------------------------------
+
+#Variables-------------------------------------------------
+
+length = len(Text)
+
+d = {}
+
+#-----------------------------------------------------------
+for i in range (length - k + 1):
+	kmer = Text[i:i+k]
+	if kmer in d:
+		d[kmer] += 1
+	else:
+		d[kmer] = 1
+
+max_freq = max(d.values())
+
+print max_freq
+answer = [kmer for kmer, freq in d.items() if freq == max_freq]
+print answer
+#print(' '.join(answer))
+
+
+
+
+
